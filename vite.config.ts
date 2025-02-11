ed91262c4a8cf6e5cddd779866b25d48a2886763
norikenpi <norikenpi@gmail.com>
2025-02-12 01:50:32 +0900

    add gh pages setting

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/react-tailwind/",
   root: ".", // プロジェクトのルート
   server: {
     port: 3000, // 開発サーバーのポート番号
